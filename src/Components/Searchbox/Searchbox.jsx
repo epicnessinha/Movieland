d82ba8b6ecdea84423c53abadeb604589484b305
vanessaferreira <vanessa.ferreira@xing.com>
2022-11-16 15:31:26 +0000

    Fixing some bugs

--- src/Components/Searchbox/Searchbox.jsx
+++ src/Components/Searchbox/Searchbox.jsx
@@ -1,25 +1,23 @@
 import React from 'react'
 import './Searchbox.css';
-//import logo from '../../img/logo.svg'
 import { useNavigate } from 'react-router-dom';
 
 
 const Searchbox = () => {
   let navigate = useNavigate();
 
-
-  
   return (
-    <div className="App">
-    <header className='App-Title'>
-      <h1>The Epic Movieland</h1>
-    </header>
-    <div className='rightside'>
+    <div className="searchBoxDesign">
+    <div className='left'>
+      <img className='logo' src={logo} alt="logo"></img>
+    <div className="webAppName"><h1>The Epic Movieland</h1></div>
+      </div>
+    <div className='right'>
       <div className='linkDesign' onClick={()=>setTimeout(()=>{navigate("/")},500)}>Top Movies</div>
       <div className='linkDesign' onClick={()=>setTimeout(()=>{navigate("/search")},500)}>Premium Search</div>
     </div>
     <main>
-   
+       <h2>INSERIR A SEARCHBOX AQUI!</h2>
     </main>
   </div> 
   )
