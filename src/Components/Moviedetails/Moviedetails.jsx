fbfa5b412f2a5a328a61642fa4c9371a6de92821
Vanessa Ferreira <vanessa.ferreira.dev@gmail.com>
2022-11-17 10:32:59 +0000

    Update Moviedetails.jsx

--- src/Components/Moviedetails/Moviedetails.jsx
+++ src/Components/Moviedetails/Moviedetails.jsx
@@ -5,13 +5,16 @@ import start from '../../img/star.png'
 const Moviedetails = ({movie}) => {
 
     return (
-        <div className="movieInfoDesign">
+         <div className="movieInfoDesign">
             <h3>{movie.name}</h3>
-            <div className="moviePoster"><img src={`https://image.tmdb.org/t/p/original${movie.poster_path}` alt={movie.name} width="375" height="560"/></div>
+            <div className="moviePoster"><img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.name} width="375" height="560"/></div>
             <div className="ReleaseAndRating">
                 <div>Release: {movie.release_date}</div>
-                <div className="synopsis">{movie.overview}</div>
+            
+               <div className="rating">{movie.vote_average}<img className='star' src={start} alt='Rating Star'></img></div>
             </div>
+           <div className="synopsis">{movie.overview}</div>
+           </div>
           
           
       
