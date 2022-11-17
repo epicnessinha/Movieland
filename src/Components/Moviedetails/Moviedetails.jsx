54de549160d8b3eb003bd1c042ded88d0d392823
vanessaferreira <vanessa.ferreira@xing.com>
2022-11-17 00:02:13 +0000

    Final App

--- src/Components/Moviedetails/Moviedetails.jsx
+++ src/Components/Moviedetails/Moviedetails.jsx
@@ -20,3 +20,26 @@ const Moviedetails = ({movie}) => {
 }
 
 export default Moviedetails;
+
+
+/*const Moviedetails = ({ movie }) => {
+    const { poster_path, name, overview, release_date, vote_average } = movie;
+    const url = `https://image.tmdb.org/t/p/original${poster_path}`;
+    return (
+      <div className="movieInfoDesign">
+        <h3>{name}</h3>
+        <div className="moviePoster">
+          <img src={url} alt={name} />
+        </div>
+        <div className="ReleaseAndRating">
+          <div>Release: {release_date}</div>
+          <div className="rating">
+            {vote_average}
+            <img className="star" src={start} alt="Rating Star"></img>
+          </div>
+        </div>
+        <div className="synopsis">{overview}</div>
+      </div>
+    );
+  };
+  export default Moviedetails;*/  //Estava a desconfigurar as imagens
