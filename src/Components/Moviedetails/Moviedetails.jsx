f739e2e5c3481349886286239aa0d8a0dfb8eb44
Vanessa Ferreira <vanessa.ferreira.dev@gmail.com>
2022-11-17 10:22:02 +0000

    Update Moviedetails.jsx

--- src/Components/Moviedetails/Moviedetails.jsx
+++ src/Components/Moviedetails/Moviedetails.jsx
@@ -10,9 +10,10 @@ const Moviedetails = ({movie}) => {
             <div className="moviePoster"><img src={`https://image.tmdb.org/t/p/original${movie.poster_path}`} alt={movie.name} width="375" height="560"/></div>
             <div className="ReleaseAndRating">
                 <div>Release: {movie.release_date}</div>
+                <div className="synopsis">{movie.overview}</div>
             </div>
-           <div className="synopsis">{movie.overview}</div>
-           </div>
+          
+          
       
     )
 }
